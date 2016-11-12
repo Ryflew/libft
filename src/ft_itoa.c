5f0caa6e3a5d388300850ec438ae15d44addc74f
Victor DARMAYAN <vdarmaya@e3r13p6.42.fr>
2016-11-12 18:47:30 +0100

    change itoa

--- src/ft_itoa.c
+++ src/ft_itoa.c
@@ -6,54 +6,35 @@
 /*   By: vdarmaya <vdarmaya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/07 16:52:17 by vdarmaya          #+#    #+#             */
-/*   Updated: 2016/11/09 21:18:38 by vdarmaya         ###   ########.fr       */
+/*   Updated: 2016/11/12 18:44:35 by vdarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
+#include <stdio.h>
 
-static void	ft_strconv(char *str, int nbr)
+static void	ft_strconv(char *str, int size, int nbr)
 {
 	int		i;
 
 	i = -1;
+	str[size] = '\0';
 	if (nbr < 0)
 	{
-		str[++i] = '-';
+		str[0] = '-';
 		nbr = -nbr;
 	}
 	while (nbr > 9)
 	{
-		str[++i] = (char)((nbr % 10) + 48);
+		str[--size] = (char)((nbr % 10) + 48);
 		nbr /= 10;
 	}
-	str[++i] = (char)(nbr + 48);
-	str[++i] = '\0';
-}
-
-static void	ft_strrevitoa(char *dest, char *src)
-{
-	int		len;
-	int		count;
-	int		i;
-
-	i = -1;
-	len = ft_strlen(src);
-	count = len;
-	if (src[0] == '-')
-	{
-		dest[++i] = '-';
-		count--;
-	}
-	while (count--)
-		dest[++i] = src[--len];
-	dest[++i] = '\0';
+	str[--size] = (char)(nbr + 48);
 }
 
 char		*ft_itoa(int n)
 {
-	char	*tmp;
 	char	*str;
 	int		count;
 	int		size;
@@ -69,12 +50,8 @@ char		*ft_itoa(int n)
 		count = (int)(count / 10);
 		size++;
 	}
-	tmp = (char*)malloc(sizeof(*tmp) * (size + 1));
-	str = (char*)malloc(sizeof(*tmp) * (size + 1));
-	if (!tmp || !str)
+	if (!(str = (char*)malloc(sizeof(*str) * (size + 1))))
 		return (NULL);
-	ft_strconv(tmp, n);
-	ft_strrevitoa(str, tmp);
-	free(tmp);
+	ft_strconv(str, size, n);
 	return (str);
 }
